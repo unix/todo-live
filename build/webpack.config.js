5b1a8f8efd7bb026ab51e8ebd37ac7fd256aa32b
WittBulter <nanazuimeng123@gmail.com>
2018-01-20 17:30:47 +0800

    build: init build scripts

---
+++ build/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const lintConfig = require('../tslint.json')
+const promisify = require('util').promisify
+const readDir = promisify(fs.readdir)
+const isDebug = process.env.DEBUG || false
+const manifest = require('../package.json')
+
+
+module.exports = (async() => {
+  const externals = Object.keys(manifest.dependencies).reduce((pre, next) =>
+    Object.assign({}, pre, { [next]: `require('${next}')`}), {})
+  
+  const entries = await readDir(path.join(__dirname, '../src/bin'))
+  const entryName = fileName => `bin/${fileName.split('.ts')[0]}`
+  const entriesMap = entries.reduce((pre, next) => Object.assign({},
+    pre, { [entryName(next)]: path.resolve(__dirname, `../src/bin/${next}`) }), {})
+  
+  const base = {
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: '[name].js',
+    },
+  
+    devtool: isDebug ? 'source-map' : '',
+  
+    target: 'node',
+  
+    node: {
+      __dirname: false,
+      __filename: true,
+    },
+  
+    resolve: {
+      extensions: [ '.ts', '.js'],
+      modules: [
+        path.join(__dirname, '../node_modules'),
+      ],
+    },
+  
+    module: {
+      loaders: [
+        {
+          test: /\.ts/,
+          enforce: 'pre',
+          exclude: /node_modules/,
+          loader: 'tslint-loader',
+          options: {
+            configuration: lintConfig,
+          },
+        },
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            configFile: path.resolve(__dirname, '../tsconfig.json'),
+          }
+        },
+      ],
+    },
+  
+    plugins: [
+      new webpack.BannerPlugin({
+        raw: true,
+        banner: '#!/usr/bin/env node',
+        exclude: 'index.js',
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false },
+      }),
+    ],
+  }
+  
+  const server = {
+    entry: entriesMap,
+    externals: externals,
+    target: 'node',
+  }
+  
+  return [webpackMerge(base, server)]
+})()
+
