3a63b6c2eb986973ede88db960594f03d7fcd31b
WittBulter <nanazuimeng123@gmail.com>
2018-01-20 17:31:23 +0800

    feat: init todo command
    test(mocha): init mocha test command

---
+++ src/bin/todo.ts
@@ -0,0 +1,21 @@
+import * as commander from 'commander'
+import chalk from 'chalk'
+import notifier from 'update-notifier'
+const pkg = require('../../package.json')
+
+const v: string = process.version.match(/\d+/g)[0]
+if (+v < 5) {
+  console.log(chalk.yellow('require NodeJS 6+ version.'))
+  console.log(chalk.yellow('you need to upgrade the NodeJS.\n'))
+  process.exit(1)
+}
+
+notifier({ pkg, updateCheckInterval: 1 }).notify({ isGlobal: true })
+
+commander
+  .version(pkg.version)
+  .usage('<command> [options]')
+  .parse(process.argv)
+
+  
+
---
+++ test/keep.init.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+
+describe('just keep', () => {
+  
+  it('just keep', () => {
+    expect(1).to.be.eq(1)
+  })
+  
+})
+
