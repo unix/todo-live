0495c540a6a0906df251d7367f71f50a8d98e4f2
WittBulter <nanazuimeng123@gmail.com>
2018-01-21 18:44:00 +0800

    feat(store): improve store methods

--- src/utils/store.base.ts
+++ src/utils/store.base.ts
@@ -1,13 +1,22 @@
 import File from './file'
 import Filter from './filter'
 
-export type FindExp = {
-  [key: string]: string,
+export type StoreQueryValue = {
+  // query key: $lt, $lte, $gt, $gte, $ne
+  [key: string]: string | number
+}
+
+export type StoreQuery = {
+  [key: string]: string | StoreQueryValue,
+}
+
+export type FileKeyValue = {
+  [key: string]: any,
 }
 
 export interface UserStore {
-  find: (key: any) => any[],
-  findOne: (key) => any,
+  find: (query: StoreQuery) => any[],
+  findOne: (query: StoreQuery) => any,
   findAll: () => any[],
   save: (key: string, value: any) => void,
 }
@@ -15,11 +24,17 @@ export interface UserStore {
 export class StoreBase {
   
   readonly root: string = Filter.path(`${__dirname}../../.storage`)
-  private database: string = 'todo'
+  private database: string = 'list'
   private url: string
   
   static CheckWorkEnv(root: string): void {
     !File.exists(root) && File.spawnSync('mkdir', [root])
+  }
+  
+  static queryOperator(oper: string): string {
+    return {
+      '$lt': '<', '$lte': '<=', '$gt': '>', '$gte': '>=', '$ne': '!==',
+    }[oper]
   }
   
   constructor(database: string) {
@@ -33,8 +48,27 @@ export class StoreBase {
     return this
   }
   
+  protected async getFile(): FileKeyValue[] {
+    try {
+      const fileContent: string = await File.readFile(this.url, 'utf-8')
+      const files: FileKeyValue[] = JSON.parse(fileContent || '[]')
+      return files
+    } catch (e) {
+      return []
+    }
+  }
+  
+  protected async setFile(file: FileKeyValue = {}): void {
+    try {
+      const files = await this.getFile()
+      const fileContent: string = JSON.stringify(files.concat(file))
+      await File.writeFile(this.url, fileContent, 'utf-8')
+    } catch (e) {
+    }
+  }
+  
   private init(): void {
-    const path = Filter.path(`${this.root}/${this.database}.json`)
+    const path = Filter.path(`${this.root}/todo_live_${this.database}.json`)
     if (File.exists(path)) return (this.url = path)
     File.spawnSync('touch', [path])
     this.url = path
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,4 +1,4 @@
-import { StoreBase, UserStore } from './store.base'
+import { StoreBase, UserStore, FileKeyValue, StoreQuery } from './store.base'
 
 export default class Store extends StoreBase implements UserStore {
   
@@ -6,20 +6,51 @@ export default class Store extends StoreBase implements UserStore {
     super(database)
   }
   
-  find(): any {
-  
+  async find(query: StoreQuery): any[] {
+    const contents: FileKeyValue[] = await this.getFile()
+    if (!contents || !contents.length) return []
+    const queryFilters: Function[] | null = this.parseQuery(query)
+    if (!queryFilters) return contents
+    return contents.filter(v => queryFilters(v))
   }
   
-  findAll(): any {
-  
+  async findAll(): any {
+    const contents: FileKeyValue[] = await this.getFile()
+    if (!contents || !contents.length) return []
+    return contents
   }
   
-  findOne(): any {
-  
+  async findOne(query: StoreQuery): any {
+    const contents: FileKeyValue[] = await this.getFile()
+    if (!contents || !contents.length) return {}
+    
+    const queryFilters: Function[] | null = this.parseQuery(query)
+    if (!queryFilters) return contents[0]
+    
+    const val: any = contents.find(v => queryFilters(v))
+    return val || {}
   }
   
-  save(): any {
+  async save(document: any = null): any {
+    if (!document) return
+    if (!document._id) {
+      document._id = Math.random().toString(16).slice(-12)
+    }
+    await this.setFile(document)
+  }
   
+  private parseQuery(query: StoreQuery = {}): Function[] | null {
+    const queryKeys: string[] = Object.keys(query)
+    if (!queryKeys.length) return null
+    let queryVal: any, type: string
+    return queryKeys.map(key => {
+      queryVal = query[key]
+      type = typeof queryVal
+      if (type !== 'object') return v => v[key] && v[key] === queryVal
+      const operKey: string = Object.keys(queryVal || {})[0]
+      const oper: string = StoreBase.queryOperator(operKey)
+      return v => v[key] && eval(`${v[key]} ${oper} ${queryVal[operKey]}`)
+    })
   }
   
 }
