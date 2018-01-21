6860c1ca16a0e576bec13e2511af6b6ffeb8ca46
WittBulter <nanazuimeng123@gmail.com>
2018-01-21 19:33:59 +0800

    test(store): improve store testcase

--- test/utils/store.ts
+++ test/utils/store.ts
@@ -1,9 +1,52 @@
 import { expect } from 'chai'
+import { Store } from '../../src/utils/store'
 
 describe('Storage actions', () => {
   
-  it('should return todo live store', async() => {
+  let TestStore
+  before(() => TestStore = new Store('test'))
+  
+  it('should return saved object', async() => {
+    await TestStore.save({ test_key: 'test' })
+    const result = await TestStore.findOne()
+    expect(result).to.be.include({ test_key: 'test' })
+    TestStore.removeAll()
   })
+  
+  it('should return specified query results', async() => {
+    await TestStore.save({ name: 'test1' })
+    await TestStore.save({ name: 'test2' })
+    const result = await TestStore.findOne({ name: 'test2' })
+    expect(result).to.be.include({ name: 'test2' })
+    TestStore.removeAll()
+  })
+  
+  it('should auto improve key of _id', async() => {
+    await TestStore.save({ name: 'test1' })
+    const result = await TestStore.findOne({ name: 'test1' })
+    expect(result).to.contains.keys('_id')
+    TestStore.removeAll()
+  })
+  
+  it('should return group data', async() => {
+    await TestStore.save({ name: 'test1' })
+    await TestStore.save({ name: 'test2' })
+    const results = await TestStore.find()
+    expect(results).to.a('array').lengthOf(2)
+    TestStore.removeAll()
+  })
+  
+  it('should find empty object', async() => {
+    const result = await TestStore.findOne({ name: 'empty' })
+    expect(result).to.a('object')
+  })
+  
+  it('should find empty array', async() => {
+    const result = await TestStore.find({ name: 'empty' })
+    expect(result).to.a('array').lengthOf(0)
+  })
+  
+  after(() => TestStore.implode())
   
 })
 
