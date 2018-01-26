d005ee5e58ddbf60dc3757ed9b8d1fd90c8739fb
WittBulter <nanazuimeng123@gmail.com>
2018-01-26 19:09:10 +0800

    feat(filter): append str-to-time func

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,12 +1,32 @@
 
-const pathFormat = (path: string): string => path.replace(/\\/g, '/')
+export const pathFormat = (path: string): string => path.replace(/\\/g, '/')
 
-const strEllipsis = (str: string, len: number): string => {
+export const strEllipsis = (str: string, len: number): string => {
   if (str.length <= len) return str
   return `${str.substr(0, len)}...`
 }
 
-export default {
-  path: pathFormat,
-  strEllipsis,
+export const strToTime = (str: string) => {
+  // is hour, not include text
+  if (!Number.isNaN(+str)) {
+    const h = new Date().getHours()
+    return new Date().setHours(+str + h)
+  }
+  
+  // is minute
+  if (/m/.test(str)) {
+    str = str.replace(/m/g, '')
+    const m = new Date().getMinutes()
+    return Number.isNaN(+str) ? 0 : new Date().setMinutes(+str + m)
+  }
+  
+  // is day
+  if (/d/.test(str)) {
+    str = str.replace(/d/g, '')
+    const d = new Date().getDay()
+    return Number.isNaN(+str) ? 0 : new Date().setDate(+str + d)
+  }
+  
+  // wrong
+  return 0
 }
